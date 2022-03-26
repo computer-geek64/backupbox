fc8832b96b95478ca48e3826a1c475025ee61bd9
Ashish D'Souza <sudouser512@gmail.com>
2022-03-26 15:02:11 -0400

    Added Windows backup

--- samba/run.sh
+++ samba/run.sh
@@ -2,11 +2,11 @@
 # run.sh
 
 docker build -t samba .
-docker create --network=host -v "/data/nas/family:/data/family" -v "/data/nas/users:/home" --restart=unless-stopped --name samba samba
+docker create --network=host -v "/data/nas/samba/passdb.tdb:/var/lib/samba/private/passdb.tdb" -v "/data/nas/family:/data/family" -v "/data/nas/users:/home" -v "/data/nas/windows_backup:/data/windows_backup" --restart=unless-stopped --name samba samba
 docker container start samba
 
 # Add Samba users with:
-# docker container exec -it samba ./create_samba_user.sh [user]
+# docker container exec -it samba ./create_samba_user.sh [user] [password]
 
 # Delete Samba users with:
 # docker container exec -it samba ./delete_samba_user.sh [user]
--- samba/src/create_samba_user.sh
+++ samba/src/create_samba_user.sh
@@ -2,5 +2,5 @@
 # create_samba_user.sh
 
 useradd -ms /bin/false "$1"
-smbpasswd -a "$1"
+(echo "$2"; echo "$2") | smbpasswd -s -a "$1"
 usermod -aG family "$1"
